0dc7aa299f4d7d25f1318052cfc236c6ce28c553
AlexanderTheGreat <goulash93@gmail.com>
2014-11-13 00:21:47 -0500

    Enabled Speech Recognizer button on phone

--- mobile/src/main/java/com/example/alexanderthegreat/medscribe/MedScribePhone.java
+++ mobile/src/main/java/com/example/alexanderthegreat/medscribe/MedScribePhone.java
@@ -1,17 +1,34 @@
 package com.example.alexanderthegreat.medscribe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 
 public class MedScribePhone extends Activity {
+
+    Button recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_scribe_phone);
+
+        recordButton = (Button) findViewById(R.id.button_medscribe_record);
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySpeechRecognizer();
+            }
+        });
     }
 
 
@@ -33,4 +50,32 @@ public class MedScribePhone extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
+
+
